6309aa0949ff9288dec762c6f69dcb8b344ebdaa
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-08 11:13:12 +0200

    Test bundler

--- dylan_tests2.rb
+++ dylan_tests2.rb
@@ -1,41 +1,42 @@
 # require 'pry-byebug'
+require 'rubocop'
 
-# # Implicitly defined subject
+# Implicitly defined subject
 
-# # subject exposed in top level group
-# describe Array do
-#   it 'should be empty when first created' do
-#     # subject.should be_empty
-#     expect(subject).to be_empty
-#   end
-# end
-
-# # subject in a nested group
-# describe Array do
-#   describe 'when first created' do
-#     it 'should be empty' do
-#       # subject.should be_empty``
-#       expect(subject).to be_empty
-#     end
-#   end
-# end
-
-# subject in a nested group with a different class (outermost wins)
-class ArrayWithOneElement < Array
-  def initialize(*)
-    super
-    unshift 'first element'
+# subject exposed in top level group
+describe Array do
+  it 'should be empty when first created' do
+    # subject.should be_empty
+    expect(subject).to be_empty
   end
 end
 
+# subject in a nested group
 describe Array do
+  describe 'when first created' do
+    it 'should be empty' do
+      # subject.should be_empty``
+      expect(subject).to be_empty
+    end
+  end
+end
+
+# subject in a nested group with a different class (innermost wins)
+class ArrayWithOneElement < Array
+  def initialize(*)
+    super
+    unshift "first element"
+  end
+end
+
+RSpec.describe Array do
   describe ArrayWithOneElement do
-    context 'referenced as subject' do
-      it 'should be empty (because it is the Array declared at the top)' do
-        # binding.pry
-        # subject.should be_empty
-        expect(subject).to be_empty
+    context "referenced as subject" do
+      it "contains one element" do
+        expect(subject).to include("first element")
       end
     end
   end
 end
+
+puts "rubocop version loaded: #{Gem.loaded_specs['rubocop'].version}"
