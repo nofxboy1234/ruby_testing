6bb743235f0c4fba909885a75205c51d6cd05501
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-10 12:53:01 +0200

    Complete exercise 14

--- lib/14_find_number.rb
+++ lib/14_find_number.rb
@@ -11,4 +11,21 @@ class FindNumber
     @answer = answer.value
     @guess = guess
   end
+
+  def make_guess
+    array = [min, max]
+    array.sum / array.size
+  end
+
+  def game_over?
+    guess == answer
+  end
+
+  def update_range
+    if guess < answer
+      @min = guess + 1
+    elsif guess > answer
+      @max = guess - 1
+    end
+  end
 end
