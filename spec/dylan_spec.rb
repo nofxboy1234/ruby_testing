1ba3a18ad0cf4ea8cb4e7e4127fcb90197357a6d
Dylan Palmboom <nofxboy1234@gmail.com>
2023-05-10 15:19:54 +0200

    Test before(:each)

--- spec/dylan_spec.rb
+++ spec/dylan_spec.rb
@@ -32,18 +32,47 @@
 # end
 
 
-$count = 0
-describe "let!" do
-  invocation_order = []
+# $count = 0
+# describe "let!" do
+#   invocation_order = []
 
-  let!(:count) do
-    invocation_order << :let!
-    $count += 1
+#   let!(:count) do
+#     invocation_order << :let!
+#     $count += 1
+#   end
+
+#   it "calls the helper method in a before hook" do
+#     invocation_order << :example
+#     expect(invocation_order).to eq([:let!, :example])
+#     expect(count).to eq(1)
+#   end
+# end
+
+
+class User
+  def tests
+    @tests ||= []
+  end
+end
+
+describe User do
+  before(:each) do
+    @user = User.new
   end
 
-  it "calls the helper method in a before hook" do
-    invocation_order << :example
-    expect(invocation_order).to eq([:let!, :example])
-    expect(count).to eq(1)
+  describe "initialized in before(:each)" do
+    it "has 0 tests" do
+      # @user.should have(0).tests
+      expect(@user.tests.size).to eq(0)
+    end
+
+    it "can accept new tests" do
+      @user.tests << Object.new
+    end
+
+    it "does not share state across examples" do
+      # @user.should have(0).tests
+      expect(@user.tests.size).to eq(0)
+    end
   end
 end
